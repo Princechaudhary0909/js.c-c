1dc78993c72b202e218da4d0979020df68af9da3
Princechaudhary0909 <princechaudhary0137@gmail.com>
2024-07-21 16:54:55 +0000

    datatype

---
+++ javascript/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//TREAT ALL JS CODE AS NEWWE VERSION
+
+
+// alert("hello prince" )// we are using nodejs not browser
+
+// code readability should be high
+
+
+//number => 2 to the power 53
+//bigint
+
+
+//string =>""
+//boolean => true/false
+// null => stand alone value empty value
+//undefined => not given value
+// symbol => to find unique ness
+
+
+// OBJECT
+
+console.log(typeof "");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
