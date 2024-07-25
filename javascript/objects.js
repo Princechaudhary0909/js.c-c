21f4f23f29b3300a843bab59019a2fb645ecfddf
Princechaudhary0909 <princechaudhary0137@gmail.com>
2024-07-25 12:49:57 +0000

    objects in js

---
+++ javascript/objects.js
@@ -0,0 +1,49 @@
+//OBJECTS AND events ARE VERY IMPORTANT IN JAVASCRIPT 
+//objects can be defined in 2 ways 1.. literal ki tarah 2..constructor ki tarah
+//SINGLETON OBJECT  => jb humm literals ki tarah define krte hai to sigleton nhi bnta hai
+//constructor se singleton bnega
+//Object.create => this is how we create singleton object or constructor method
+
+
+
+//ibject literals
+
+// THIS SYMBOL IS VERY IMPORANT FOR INTERVIEW
+const mysym = Symbol("Key1")
+
+
+const jsuser = {
+    name: "prince",
+    "fullName": "prince Chaudhary",//we cann't access it by .(dot) method
+    //mysym:"myKey1",// we cannot use it as symbol like this
+    [mysym]:"myKey1",//now it is used as symbol
+    age: 24,
+    location : "ghaziabad",
+    email: "prince@gmail.com",
+    isLoggedIn: false,
+    lastLogIn: ["monday","sunday"]
+
+}
+console.log(jsuser.email);
+//console.log(jsuser."fullName");//not possible
+console.log(jsuser["fullName"]);
+console.log(jsuser["email"]);
+console.log( jsuser[mysym]);//so it is not used as symbol
+
+// to change value
+jsuser.email = "prince1@gmail.com........."
+//Object.freeze(jsuser);//so any changes cann't be made
+jsuser.email = "prince@microsoft.com";
+console.log(jsuser);
+
+
+
+// to add function in object
+jsuser.greeting = function(){
+    console.log("hello i'm function & how are u");
+}
+jsuser.greeting1 = function(){
+    console.log(`hello jsuser => ${this.name}`);
+}
+console.log(jsuser.greeting());
+console.log(jsuser.greeting1());
---
+++ javascript/objects2.js
@@ -0,0 +1,75 @@
+//singleton or constructor declaration of object
+const instaUser = new Object()
+ instaUser.id ="12233333",
+ instaUser.name = "prince",
+ instaUser.IsLoggedIn = true
+
+//console.log(instaUser);
+
+const regularUser = {
+    email: "prince22.com",
+    userFullName:{
+        FirstName: "prince",
+        LastName: "chaudhary"
+
+    }
+}
+// console.log(regularUser);
+// console.log(regularUser.userFullName);
+// console.log(regularUser.userFullName.FirstName);
+ 
+const obj1 = {
+    1:"a",
+    2:"b",
+}
+const obj2 = {
+    3:"c",
+    4:"d",
+}
+//const obj3 = Object.assign(obj1,obj2);
+//const obj3 = Object.assign({} ,obj1,obj2);//empty array lelete hai so that usme value store ho jaye agr nhi lenge to sb obj1 mei n store hoga
+// we will be using this mostly
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+//when values comes fron database
+const user = [{
+    id:1,
+    email:"p@gmail.com"
+
+},
+{
+    id: 1,
+    email:"pc@gmail.com"
+
+},
+{
+    id:1,
+    email:"p@gmail.com"
+
+}, {
+    id:1,
+    email:"p@gmail.com"
+
+}]
+console.log(user[0]);
+
+// console.log(instaUser);
+// console.log(Object.keys(instaUser));//iska return type hoga ARRAY so baad mein isme loops lga skte hai baaki saare array ke operations use kr skte ahi
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+
+//jb hum object mein loopthrough kr rhe hote hai to vo value exist nhi krti hai
+//so we can check that by kyoki crash ke chances hai
+console.log(instaUser.hasOwnProperty('IsLoggedIn'));
+console.log(instaUser.hasOwnProperty('IsLogged'));
+
+
+
+
+
+
+
+
+
+
