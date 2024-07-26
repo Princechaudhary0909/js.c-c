350f6adfb23d6b19744253a3e690c586ca049df4
Princechaudhary0909 <princechaudhary0137@gmail.com>
2024-07-26 06:37:20 +0000

    JSON used in API

---
+++ javascript/object2.js
@@ -0,0 +1,34 @@
+//DESTRUCTURING
+const course = {
+    coursename: "jshindi",
+    coursefees: "1000",
+    coursechannel: "c&c"
+}
+// console.log( course.coursechannel);
+const{coursechannel: channel} = course
+console.log(channel);
+
+//react destructuring
+
+const navbar = () => {
+
+}
+navbar(company = "prince")
+
+
+
+// concept of API
+//JSON 
+/*
+{
+    "name": "prince",
+    "coursename": "js in hindi",
+    "price":"free"
+}*/
+
+[
+    {},
+    {},
+    {}
+   
+]
